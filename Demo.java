af41923bebd302e908b2c328ce9632e199b503fe
Dixit1234567 <k97161204@gmail.com>
2023-08-14 06:51:19 +0530

    Encapsulation

--- Demo.java
+++ Demo.java
@@ -1,39 +1,39 @@
 class Encap {
-    private String name; // Added missing semicolons
+    private String name; 
     private int roll;
     private int age;
 
     public int getAge() {
-        return age; // Corrected variable name and added missing return type
+        return age; 
     }
 
     public int getRoll() {
-        return roll; // Corrected variable name
+        return roll; 
     }
 
     public String getName() {
-        return name; // Corrected variable name
+        return name; 
     }
 
     public void setAge(int a) {
-        age = a; // Corrected variable name
+        age = a; 
     }
 
     public void setRoll(int r) {
-        roll = r; // Corrected variable name
+        roll = r; 
     }
 
     public void setName(String n) {
-        name = n; // Corrected variable name
+        name = n; 
     }
 }
 
 class Demo {
-    public static void main(String args[]) { // Corrected 'Static' to 'static'
+    public static void main(String args[]) {
         Encap e = new Encap();
-        e.setName("Dixit"); // Corrected method names to camelCase
-        e.setRoll(10224);   // Corrected method names to camelCase
-        e.setAge(10);       // Corrected method names to camelCase
+        e.setName("Dixit"); 
+        e.setRoll(10224);   
+        e.setAge(10);      
         System.out.println("Name: " + e.getName());
         System.out.println("Age: " + e.getAge());
         System.out.println("Roll: " + e.getRoll());
